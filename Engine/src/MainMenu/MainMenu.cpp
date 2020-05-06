037c2a2ec72e903c14a4efb03ca684c05ad93c78
popovic-olivera <olja997@hotmail.com>
2020-05-06 12:08:26 +0200

    Main Menu logic moved to separate class

---
+++ Engine/src/MainMenu/MainMenu.cpp
@@ -0,0 +1,64 @@
+#include "precomp.h"
+#include "MainMenu.h"
+#include "Render/Renderer.h"
+
+#include <SDL.h>
+
+bool Engine::MainMenu::Init()
+{
+    LOG_INFO("Initializing Main Menu");
+
+    int retVal = SDL_CreateWindowAndRenderer(800, 600, 0, &m_MenuWindow, &m_MenuRenderer);
+    SDL_SetWindowTitle(m_MenuWindow, "Vampelvis");
+
+	return !retVal;
+}
+
+void Engine::MainMenu::Update(Renderer* windowRenderer_)
+{
+    if (!isVisible) ShowMenu(windowRenderer_);
+  
+    SDL_SetRenderDrawColor(m_MenuRenderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(m_MenuRenderer);
+    SDL_RenderPresent(m_MenuRenderer);
+}
+
+void Engine::MainMenu::Shutdown()
+{
+    LOG_INFO("Shutting down Menu Renderer");
+
+    if (m_MenuRenderer != nullptr)
+    {
+        SDL_DestroyRenderer(m_MenuRenderer);
+    }
+
+    m_MenuRenderer = nullptr;
+
+    LOG_INFO("Shutting down Menu Window");
+
+    if (m_MenuWindow != nullptr)
+    {
+        SDL_DestroyWindow(m_MenuWindow);
+    }
+
+    m_MenuWindow = nullptr;
+}
+
+void Engine::MainMenu::ShowMenu(Renderer* windowRenderer_)
+{
+    windowRenderer_->HideWindow();
+
+    SDL_ShowWindow(m_MenuWindow);
+    SDL_RaiseWindow(m_MenuWindow);
+
+    isVisible = true;
+}
+
+void Engine::MainMenu::HideMenu(Renderer* windowRenderer_)
+{
+    SDL_HideWindow(m_MenuWindow);
+
+    windowRenderer_->ShowWindow();
+
+    isVisible = false;
+}
---
+++ Engine/src/MainMenu/MainMenu.h
@@ -0,0 +1,26 @@
+#pragma once
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+namespace Engine
+{
+	class Renderer;
+
+	class MainMenu
+	{
+	public:
+		bool Init();
+		void Update(Renderer* windowRenderer_);
+		void Shutdown();
+
+		void ShowMenu(Renderer* windowRenderer_);
+		void HideMenu(Renderer* windowRenderer_);
+
+	private:
+		SDL_Window* m_MenuWindow;
+		SDL_Renderer* m_MenuRenderer;
+
+		bool isVisible{ true };
+	};
+}
