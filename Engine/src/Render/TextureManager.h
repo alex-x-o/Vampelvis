a4c20d6600f80e77270ced43615590159661c150
alex-x-o <mi16048@alas.matf.bg.ac.rs>
2020-05-09 03:23:15 +0200

    Started changing Texture Manager

--- Engine/src/Render/TextureManager.h
+++ Engine/src/Render/TextureManager.h
@@ -8,12 +8,14 @@ namespace Engine
     class TextureManager
     {
     public:
-        bool CreateTexture(Renderer* renderer_, std::string name_, std::string path_);
+        bool CreateTexture(Renderer* renderer_, int lvlId_, std::string name_, std::string path_);
         Texture* GetTexture(std::string name_);
+        void WriteTextures();
 
         TextureManager() = default;
     private:
         std::unordered_map<std::string, std::unique_ptr<Texture>> m_Textures{};
+        std::unordered_multimap<int, std::unordered_map<std::string, std::unique_ptr<Texture>>*> m_Multi{};
 
         TextureManager(const TextureManager& other) = delete;
         TextureManager& operator=(TextureManager& other) = delete;
