a6e824e48d0143d7b6a97c07da5fd486d36feee6
Konstantin <konstantin.klima@live.com>
2020-05-11 12:31:08 +0200

    Refactored and added new methods to TextureManager

--- Engine/src/Render/TextureManager.cpp
+++ Engine/src/Render/TextureManager.cpp
@@ -4,6 +4,8 @@
 #include "Render/Texture.h"
 #include "Render/Renderer.h"
 
+#include <random>
+
 namespace Engine
 {
     bool TextureManager::CreateTexture(Renderer* renderer_, int lvlId_, int categoryId_, std::string name_, std::string path_)
@@ -48,131 +50,124 @@ namespace Engine
 
     Texture* TextureManager::GetCommonTexture(int categoryId_, std::string name_)
     {
-        auto levelFinder = m_LevelMap.find(0);
-        if (levelFinder == m_LevelMap.end()) {
-            LOG_ERROR("Attempting to get a texture from a level that doesn't exist! level: {} (Common)", 0);
-            return nullptr;
-        }
-        auto& levelMap = levelFinder->second;
-
-        auto categoryFinder = levelMap.find(categoryId_);
-        if (categoryFinder == levelMap.end()) {
-            LOG_ERROR("Attempting to get a texture from a category that doesn't exist! category: {}", categoryId_);
-            return nullptr;
-        }
-        auto& categoryMap = categoryFinder->second;
-
-        auto textureDictFinder = categoryMap.find(name_);
-        if (textureDictFinder == categoryMap.end()) {
-            LOG_ERROR("Attempting to get an unknown texture! name: {}", name_);
-            return nullptr;
-        }
-
-        int id = textureDictFinder->second;
-        auto textureFinder = m_TextureStore.find(id);
-        if (textureFinder == m_TextureStore.end()) {
-            LOG_ERROR("Texture {} not found with expected id: {}", name_, id);
-            return nullptr;
-        }
-
-
-        return m_TextureStore.at(id).get();
-    }
-
-    /*
-    bool TextureManager::CreateTexture(Renderer* renderer_, int categoryId_, std::string name_, std::string path_)
-    {
-        auto textureMap = m_LevelStore[0][categoryId_];
-        if (textureMap.find(name_) != textureMap.end()) {
-            LOG_ERROR("Attempting to create a texture twice! name: {}, path: {}", name_, path_);
-            return false;
-        }
-        textureMap.emplace(name_, std::make_unique<Texture>(renderer_, path_));
-
-
-        return true;
+        return GetTextureLevelTextureInternal(0, categoryId_, name_, true);
     }
 
     Texture* TextureManager::GetLevelTexture(int levelId_, int categoryId_, std::string name_)
     {
-        auto levelFinder = m_LevelStore.find(levelId_);
-        if (levelFinder == m_LevelStore.end()) {
-            LOG_ERROR("Attempting to get a texture from a level that doesn't exist! level: {}", levelId_);
-            return nullptr;
-        }
-        auto categoryMap = levelFinder->second;
-
-        auto categoryFinder = categoryMap.find(categoryId_);
-        if (categoryFinder == categoryMap.end()) {
-            LOG_ERROR("Attempting to get a texture from a category that doesn't exist! category: {}", categoryId_);
-            return nullptr;
-        }
-        auto textureMap = categoryFinder->second;
-
-        if (textureMap.find(name_) == textureMap.end()) {
-            LOG_ERROR("Attempting to get a texture that doesn't exist! name: {}", name_);
-            return nullptr;
-        }
-
-        return textureMap.at(name_).get();
+        return GetTextureLevelTextureInternal(levelId_, categoryId_, name_, true);
     }
 
     Texture* TextureManager::GetLevelOrCommonTexture(int levelId_, int categoryId_, std::string name_)
     {
-        auto levelFinder = m_LevelStore.find(levelId_);
-        if (levelFinder == m_LevelStore.end()) {
-            levelId_ = 0;
+        auto texture = GetTextureLevelTextureInternal(levelId_, categoryId_, name_, false);
+        if (texture == nullptr)
+        {
+            texture = GetTextureLevelTextureInternal(0, categoryId_, name_, true);
         }
 
-        levelFinder = m_LevelStore.find(levelId_);
-        if (levelFinder == m_LevelStore.end()) {
-            LOG_ERROR("Attempting to get a texture from a level that doesn't exist! level: {}", levelId_);
-            return nullptr;
-        }
-
-
-        auto categoryMap = levelFinder->second;
-
-        auto categoryFinder = categoryMap.find(categoryId_);
-        if (categoryFinder == categoryMap.end()) {
-            LOG_ERROR("Attempting to get a texture from a category that doesn't exist! category: {}", categoryId_);
-            return nullptr;
-        }
-        auto textureMap = categoryFinder->second;
-
-        if (textureMap.find(name_) == textureMap.end()) {
-            LOG_ERROR("Attempting to get a texture that doesn't exist! name: {}", name_);
-            return nullptr;
-        }
-
-        return textureMap.at(name_).get();
+        return texture;
     }
 
-    Texture* TextureManager::GetLevelTexture(int categoryId_, std::string name_)
+    Texture* TextureManager::GetRandomTexture(int levelId_, int categoryId_)
     {
-        auto levelFinder = m_LevelStore.find(0);
-        if (levelFinder == m_LevelStore.end()) {
-            LOG_ERROR("Attempting to get a texture from a level that doesn't exist! level: {} (common)", 0);
+        auto textureDict = GetTextureDict(levelId_, categoryId_, true);
+        if (textureDict == nullptr)
+        {
+            LOG_ERROR("Texture dict not found");
             return nullptr;
         }
-        auto categoryMap = levelFinder->second;
+            
+
+        int size = static_cast<int>(textureDict->size());
+        if (size == 0)
+        {
+            LOG_ERROR("No textures in category {}!", categoryId_);
+            return nullptr;
+        }
+
+        std::random_device rd;
+        std::uniform_int_distribution<> randomIndex(0, size - 1);
+
+        int texIndex = randomIndex(rd);
+        int currentIndex = 0;
+
+        Texture* texture = nullptr;
+
+        for (auto [key, value] : *textureDict)
+        {
+            if(currentIndex == texIndex)
+            {
+                texture = GetTextureFromStore(value, key, true);
+                break;
+            }
+            currentIndex++;
+        }
+
+        return texture;
+    }
+
+    Texture* TextureManager::GetTextureLevelTextureInternal(int levelId_, int categoryId_, std::string name_, bool shouldLog_)
+    {
+        auto textureDict = GetTextureDict(levelId_, categoryId_, shouldLog_);
+        if (textureDict == nullptr)
+            return nullptr;
+
+        auto textureDictFinder = textureDict->find(name_);
+        if (textureDictFinder == textureDict->end()) {
+            if(shouldLog_)
+            {
+                LOG_ERROR("Attempting to get an unknown texture! name: {}", name_);
+            }
+
+            return nullptr;
+        }
+
+        int id = textureDictFinder->second;
+
+        return GetTextureFromStore(id, name_, true);
+    }
+
+    TextureManager::TextureDictionary* TextureManager::GetTextureDict(int levelId_, int categoryId_, bool shouldLog_)
+    {
+        auto levelFinder = m_LevelMap.find(levelId_);
+        if (levelFinder == m_LevelMap.end()) {
+            if (shouldLog_)
+            {
+                LOG_ERROR("Attempting to get a texture from a level that doesn't exist! level: {}", levelId_);
+            }
+
+            return nullptr;
+        }
+        auto& categoryMap = levelFinder->second;
 
         auto categoryFinder = categoryMap.find(categoryId_);
         if (categoryFinder == categoryMap.end()) {
-            LOG_ERROR("Attempting to get a texture from a category that doesn't exist! category: {}", categoryId_);
-            return nullptr;
-        }
-        auto textureMap = categoryFinder->second;
+            if (shouldLog_)
+            {
+                LOG_ERROR("Attempting to get a texture from a category that doesn't exist! category: {}", categoryId_);
+            }
 
-        if (textureMap.find(name_) == textureMap.end()) {
-            LOG_ERROR("Attempting to get a texture that doesn't exist! name: {}", name_);
             return nullptr;
         }
 
-        return textureMap.at(name_).get();
+        TextureDictionary* textureDict = &categoryFinder->second;
+        
+        return textureDict;
     }
 
-    */
+    Texture* TextureManager::GetTextureFromStore(int id_, std::string name_, bool shouldLog_)
+    {
+        auto textureFinder = m_TextureStore.find(id_);
+        if (textureFinder == m_TextureStore.end()) {
+            if (shouldLog_)
+            {
+                LOG_ERROR("Texture {} not found with expected id: {}", name_, id_);
+            }
+            return nullptr;
+        }
 
 
+        return m_TextureStore.at(id_).get();
+    }
 }
